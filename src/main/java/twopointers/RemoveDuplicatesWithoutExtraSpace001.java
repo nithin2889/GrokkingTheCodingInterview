5e014f5a80b93dd4673d24e7ce3c3ed895ccb50b
akhilamartia <akhila.martia@gmail.com>
2021-01-24 12:46:19 +0530

    two pointers - remove array duplicates with no extra space

--- src/main/java/twopointers/RemoveDuplicatesWithoutExtraSpace001.java
+++ src/main/java/twopointers/RemoveDuplicatesWithoutExtraSpace001.java
@@ -2,4 +2,22 @@ package twopointers;
 
 public class RemoveDuplicatesWithoutExtraSpace001 {
 
+  public static void main(String[] args) {
+    System.out.println(RemoveDuplicatesWithoutExtraSpace001
+        .remove(new int[]{2, 3, 3, 3, 6, 9, 9}));
+    System.out.println(RemoveDuplicatesWithoutExtraSpace001
+        .remove(new int[]{2, 2, 2, 11}));
+  }
+
+  public static int remove(int[] arr) {
+    int nextNonDuplicate = 1;
+
+    for (int i = 1; i < arr.length; i++) {
+      if (arr[nextNonDuplicate - 1] != arr[i]) {
+        arr[nextNonDuplicate] = arr[i];
+        nextNonDuplicate++;
+      }
+    }
+    return nextNonDuplicate;
+  }
 }
