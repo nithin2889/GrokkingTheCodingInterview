4cd8105717900b32a256d1c40b3648f47abdcfc3
akhilamartia <akhila.martia@gmail.com>
2021-01-29 19:03:30 +0530

    recursion

--- src/main/java/recursion/HeightOfBinaryTree.java
+++ src/main/java/recursion/HeightOfBinaryTree.java
@@ -3,13 +3,16 @@ package recursion;
 public class HeightOfBinaryTree {
 
   public static int height(Node root) {
+    // base condition
     if (root == null) {
       return 0;
     }
 
+    // hypotheses
     int left = height(root.left);
     int right = height(root.right);
 
+    // induction
     return Math.max(left, right) + 1;
   }
 
