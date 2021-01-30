bb32d420440922ead02cc85a974064abdab0aca6
akhilamartia <akhila.martia@gmail.com>
2021-01-31 01:29:52 +0530

    recursion - kth symbol grammar

--- src/main/java/recursion/KthGrammarSymbol.java
+++ src/main/java/recursion/KthGrammarSymbol.java
@@ -2,4 +2,29 @@ package recursion;
 
 public class KthGrammarSymbol {
 
+  public static void main(String[] args) {
+    System.out.println("Grammar at Nth row and Kth index: " + solveKthGrammar(4, 5));
+  }
+
+  public static int solveKthGrammar(int N, int K) {
+    // Base Condition
+    if (N == 1 && K == 1) {
+      return 0;
+    }
+
+    double length = Math.pow(2, N - 1);
+    double mid = length / 2;
+
+    // Hypothesis
+    if (K <= mid) {
+      return solveKthGrammar(N - 1, K);
+    } else {
+      // Induction
+      if (solveKthGrammar(N - 1, (int) (K - mid)) == 0) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+  }
 }
