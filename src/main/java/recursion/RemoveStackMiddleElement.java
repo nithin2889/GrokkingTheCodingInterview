7572311e52dfe01c5f6310c66179387099d83b74
akhilamartia <akhila.martia@gmail.com>
2021-01-30 19:23:46 +0530

    recursion - remove stack middle element

--- src/main/java/recursion/RemoveStackMiddleElement.java
+++ src/main/java/recursion/RemoveStackMiddleElement.java
@@ -1,5 +1,37 @@
 package recursion;
 
+import java.util.Stack;
+
 public class RemoveStackMiddleElement {
 
+  public static void main(String[] args) {
+    Stack<Integer> st = new Stack<>();
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
+
+    int mid = st.size() / 2 + 1;
+    Stack<Integer> res = solve(st, mid);
+    for (Integer s : res) {
+      System.out.print(s + " ");
+    }
+  }
+
+  public static Stack<Integer> solve(Stack<Integer> st, int k) {
+    // Base condition
+    if (k == 1) {
+      st.pop();
+      return st;
+    }
+    int el = st.pop();
+
+    // Hypotheses
+    st = solve(st, k - 1);
+
+    // Induction
+    st.push(el);
+    return st;
+  }
 }
