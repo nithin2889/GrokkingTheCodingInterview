8d92cfda713c0b3e12993058d486ef8cfe56c2a6
akhilamartia <nithin2889@gmail.com>
2021-02-11 00:19:07 +0530

    DP - longest common subsequence recursion

--- src/main/java/dynamicprogramming/lcs/LCSRecursion.java
+++ src/main/java/dynamicprogramming/lcs/LCSRecursion.java
@@ -2,4 +2,24 @@ package dynamicprogramming.lcs;
 
 public class LCSRecursion {
 
+  public static void main(String[] args) {
+    String x = "abcdgh";
+    String y = "abedfhr";
+
+    int n = x.length(), m = y.length();
+    System.out.println("The max length of common subsequence is: "
+        + lcs(x, y, n, m));
+  }
+
+  private static int lcs(String x, String y, int n, int m) {
+    if (n == 0 || m == 0) {
+      return 0;
+    }
+
+    if (x.charAt(n - 1) == y.charAt(m - 1)) {
+      return lcs(x, y, n - 1, m - 1) + 1;
+    } else {
+      return Math.max(lcs(x, y, n, m - 1), lcs(x, y, n - 1, m));
+    }
+  }
 }
