80a3138bd57c0fccf82c900528f1b98d7e224b72
akhilamartia <nithin2889@gmail.com>
2021-02-12 14:45:41 +0530

    DP - Shortest Common Super Sequence using bottom-up approach

--- src/main/java/dynamicprogramming/shortestsupersequence/ShortestCommonSuperSequenceBottomUp.java
+++ src/main/java/dynamicprogramming/shortestsupersequence/ShortestCommonSuperSequenceBottomUp.java
@@ -8,6 +8,7 @@ public class ShortestCommonSuperSequenceBottomUp {
     int inputStrLength = m + n;
 
     int[][] t = new int[m + 1][n + 1];
+
     for (int i = 0; i < m + 1; i++) {
       for (int j = 0; j < n + 1; j++) {
         if (i == 0 || j == 0) {
