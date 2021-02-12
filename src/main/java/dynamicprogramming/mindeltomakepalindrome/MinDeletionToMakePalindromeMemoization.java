4776b31f2cbffe6d083e9335c4e194f46a6af4a6
akhilamartia <nithin2889@gmail.com>
2021-02-13 02:24:50 +0530

    DP - Minimum number of deletion to make a palindromic string using memoization

---
+++ src/main/java/dynamicprogramming/mindeltomakepalindrome/MinDeletionToMakePalindromeMemoization.java
@@ -0,0 +1,34 @@
+package dynamicprogramming.mindeltomakepalindrome;
+
+public class MinDeletionToMakePalindromeMemoization {
+
+  public static void main(String[] args) {
+    String s1 = "agbcba";
+    String s2 = new StringBuilder(s1).reverse().toString();
+    int m = s1.length(), n = s2.length();
+    int[][] t = new int[m + 1][n + 1];
+    for (int i = 0; i < m + 1; i++) {
+      for (int j = 0; j < n + 1; j++) {
+        t[i][j] = -1;
+      }
+    }
+    int minLength = m - lps(s1, s2, m, n, t);
+    System.out.println("The minimum deletion made in the string to make it palindromic is: "
+        + minLength);
+  }
+
+  private static int lps(String s1, String s2, int m, int n, int[][] t) {
+    if (m == 0) {
+      return 0;
+    }
+    if (n == 0) {
+      return 0;
+    }
+    if (s1.charAt(m - 1) == s2.charAt(n - 1)) {
+      t[m][n] = 1 + lps(s1, s2, m - 1, n - 1, t);
+    } else {
+      t[m][n] = Math.max(lps(s1, s2, m - 1, n, t), lps(s1, s2, m, n - 1, t));
+    }
+    return t[m][n];
+  }
+}
