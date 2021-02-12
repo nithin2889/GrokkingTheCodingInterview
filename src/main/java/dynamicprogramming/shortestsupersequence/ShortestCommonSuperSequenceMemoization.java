10de4ff8129b9cd464b4f34a500b001daf175d8e
akhilamartia <nithin2889@gmail.com>
2021-02-12 14:45:10 +0530

    DP - Shortest Common Super Sequence using memoization

--- src/main/java/dynamicprogramming/shortestsupersequence/ShortestCommonSuperSequenceMemoization.java
+++ src/main/java/dynamicprogramming/shortestsupersequence/ShortestCommonSuperSequenceMemoization.java
@@ -6,6 +6,7 @@ public class ShortestCommonSuperSequenceMemoization {
     String s1 = "AGGTAB", s2 = "GXTXAYB";
     int m = s1.length(), n = s2.length();
     int inputStrLength = m + n;
+
     int[][] t = new int[m + 1][n + 1];
     for (int i = 0; i < m + 1; i++) {
       for (int j = 0; j < n + 1; j++) {
