3a7fae0d4901d808d9f5dacb3956c9119969e3c5
akhilamartia <nithin2889@gmail.com>
2021-02-22 01:37:55 +0530

    DP - Egg dropping problem using recursion

---
+++ src/main/java/dynamicprogramming/edddroppingproblem/EggDroppingProblemRecursive.java
@@ -0,0 +1,25 @@
+package dynamicprogramming.edddroppingproblem;
+
+public class EggDroppingProblemRecursive {
+
+  public static void main(String[] args) {
+    int e = 3, f = 5;
+    System.out.println("The minimum number of attempts needed to find the critical floor is: "
+        + solve(e, f));
+  }
+
+  private static int solve(int e, int f) {
+    if (f == 0 || f == 1) {
+      return f;
+    }
+    if (e == 1) {
+      return f;
+    }
+    int min = Integer.MAX_VALUE;
+    for (int k = 1; k < f; k++) {
+      int tempAns = 1 + Math.max(solve(e - 1, k - 1), solve(e, f - k));
+      min = Math.min(min, tempAns);
+    }
+    return min;
+  }
+}
