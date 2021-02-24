4894ec2df8b29ee71d5ff27443f94a5b1c7b17df
akhilamartia <nithin2889@gmail.com>
2021-02-24 20:11:26 +0530

    Heap - print K closest numbers in an array

--- src/main/java/heaps/PrintKClosestNumbers.java
+++ src/main/java/heaps/PrintKClosestNumbers.java
@@ -1,37 +1,6 @@
 package heaps;
 
-import java.util.Comparator;
 import java.util.PriorityQueue;
-
-class Pair implements Comparator<Pair> {
-
-  int key;
-  int value;
-
-  Pair() {
-  }
-
-  Pair(int key, int value) {
-    this.key = key;
-    this.value = value;
-  }
-
-  @Override
-  public int compare(Pair a, Pair b) {
-    if (a.value == b.value) {
-      return 0;
-    } else if (a.value < b.value) {
-      return 1;
-    } else {
-      return -1;
-    }
-  }
-
-  @Override
-  public String toString() {
-    return String.valueOf(key);
-  }
-}
 
 public class PrintKClosestNumbers {
 
