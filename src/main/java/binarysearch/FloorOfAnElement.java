ae611d869a641a249ec9d0d0ab87cb8f02bf40c8
akhilamartia <nithin2889@gmail.com>
2021-03-01 20:06:46 +0530

    Binary Search - find the floor of an element

--- src/main/java/binarysearch/FloorOfAnElement.java
+++ src/main/java/binarysearch/FloorOfAnElement.java
@@ -2,4 +2,28 @@ package binarysearch;
 
 public class FloorOfAnElement {
 
+  public static void main(String[] args) {
+    int[] arr = {1, 2, 3, 4, 8, 10, 10, 12, 19};
+    int ele = 5;
+    System.out.println("Floor of the element " + ele + " is: " + floor(arr, ele));
+  }
+
+  private static int floor(int[] arr, int ele) {
+    int start = 0, end = arr.length - 1;
+    int res = 0;
+
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+
+      if (arr[mid] == ele) {
+        return mid;
+      } else if (arr[mid] < ele) {
+        res = arr[mid];
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+    return res;
+  }
 }
