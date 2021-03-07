9bd9c70d3ca8f61cfd6dd4f9117ec3c386c24543
akhilamartia <nithin2889@gmail.com>
2021-03-07 13:29:51 +0530

    sliding window - minimum sum subarray with a given sum

--- src/main/java/slidingwindow/MinimumSumSubarrayWithAGivenSum.java
+++ src/main/java/slidingwindow/MinimumSumSubarrayWithAGivenSum.java
@@ -1,10 +1,10 @@
 package slidingwindow;
 
-public class MinimumSumSubarrayWithAGivenSum003 {
+public class MinimumSumSubarrayWithAGivenSum {
 
   public static void main(String[] args) {
     System.out.println("Smallest subarray length: "
-        + MinimumSumSubarrayWithAGivenSum003.findMinSubArray(7, new int[] { 2, 1, 5, 2, 3, 2 }));
+        + MinimumSumSubarrayWithAGivenSum.findMinSubArray(7, new int[] { 2, 1, 5, 2, 3, 2 }));
   }
 
   /**
