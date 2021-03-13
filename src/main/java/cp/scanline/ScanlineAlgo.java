101935ff4598aa0d3c3ed5b6bff276db0366fc7c
akhilamartia <nithin2889@gmail.com>
2021-03-14 01:55:10 +0530

    CP - optimized solution to find the modified array for a given range

--- src/main/java/cp/scanline/ScanlineAlgo.java
+++ src/main/java/cp/scanline/ScanlineAlgo.java
@@ -6,6 +6,10 @@ public class ScanlineAlgo {
     int[] arr = {1, 5, 2, 3, 2};
     System.out.println("Brute force to get the modified array: ");
     solveBruteForce(arr, 0, 2, 3);
+
+    arr = new int[]{5, 1, 2, 3, 4};
+    System.out.println("\nOptimized solution to get the modified array: ");
+    solveUsingScanLine(arr, 0, 4, 2);
   }
 
   private static void solveBruteForce(int[] arr, int l, int r, int x) {
@@ -15,6 +19,27 @@ public class ScanlineAlgo {
 
     for (int i = 0; i < arr.length; i++) {
       System.out.print(arr[i] + " ");
+    }
+  }
+
+  private static void solveUsingScanLine(int[] arr, int l, int r, int x) {
+    // create a prefixSum array of length one more than the given array
+    int[] prefixSum = new int[arr.length + 1];
+
+    // add x to array position l and subtract x from array position r + 1
+    prefixSum[l] += x;
+    prefixSum[r + 1] -= x;
+
+    int scan = 0;
+    // add prefixSum array contents to scan and in turn add scan to the given array
+    for (int i = 0; i < prefixSum.length; i++) {
+      scan += prefixSum[i];
+      arr[i] += scan;
+    }
+
+    // finally print the modified array
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i]);
     }
   }
 }
