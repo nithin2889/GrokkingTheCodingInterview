ad1baff386f1114869cee6ad9da04f43bae07256
akhilamartia <nithin2889@gmail.com>
2021-03-14 10:47:35 +0530

    CP - optimized solution using scanline to find the modified array for a given range

--- src/main/java/cp/scanline/ScanlineAlgo.java
+++ src/main/java/cp/scanline/ScanlineAlgo.java
@@ -3,7 +3,7 @@ package cp.scanline;
 public class ScanlineAlgo {
 
   public static void main(String[] args) {
-    int[] arr = {1, 5, 2, 3, 2};
+    int[] arr = new int[]{1, 5, 2, 3, 2};
     System.out.println("Brute force to get the modified array: ");
     solveBruteForce(arr, 0, 2, 3);
 
