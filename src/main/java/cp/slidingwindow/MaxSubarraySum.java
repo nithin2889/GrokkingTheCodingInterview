40dafce640bce4d9c7d24783cfe19e56483e114a
akhilamartia <nithin2889@gmail.com>
2021-03-15 23:24:57 +0530

    CP - maximum sum subarray using prefix sum and sliding window

--- src/main/java/cp/slidingwindow/MaxSubarraySum.java
+++ src/main/java/cp/slidingwindow/MaxSubarraySum.java
@@ -15,6 +15,7 @@ public class MaxSubarraySum {
   private static int[] prefixSum(int[] arr) {
     int[] prefix = new int[arr.length];
 
+    // predetermining the prefix sum
     for (int i = 0; i < arr.length; i++) {
       if (i == 0) {
         prefix[0] = arr[0];
