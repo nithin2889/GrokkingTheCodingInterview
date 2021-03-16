d76bf88dd1f3d0673261cfb422dd0b7a6452f2ea
akhilamartia <nithin2889@gmail.com>
2021-03-16 19:29:40 +0530

    Heap - Kth smallest element in an array

--- src/main/java/heaps/KthSmallestElement.java
+++ src/main/java/heaps/KthSmallestElement.java
@@ -1,5 +1,6 @@
 package heaps;
 
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class KthSmallestElement {
@@ -8,7 +9,21 @@ public class KthSmallestElement {
     int[] arr = {7, 10, 4, 3, 20, 15};
     int k = 3;
     int size = arr.length;
-    System.out.println("The kth smallest element is: " + findKthSmallestElement(arr, k, size));
+    System.out.println("The kth smallest element using a brute force technique is: "
+        + findKthSmallestElementBruteForce(arr, k, size));
+    System.out.println("The kth smallest element using a MaxHeap is: "
+        + findKthSmallestElement(arr, k, size));
+  }
+
+  private static int findKthSmallestElementBruteForce(int[] arr, int k, int size) {
+    int item = 0;
+    Arrays.sort(arr);
+    for (int i = 0; i < size; i++) {
+      if (k > 0 && k < size) {
+        item = arr[k - 1];
+      }
+    }
+    return item;
   }
 
   private static int findKthSmallestElement(int[] arr, int k, int size) {
